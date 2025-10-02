8816aed4d96f9c84d9db0b0737ac99bc2d1437fa
Bas <basfrankenn@gmail.com>
2025-10-02 15:14:52 +0200

    Delete storage-account directory

--- storage-account/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "storage_account_id" {
-  description = "The ID of the Storage Account"
-  value       = module.storage.resource_id
-}
-
-output "storage_account_name" {
-  description = "The name of the Storage Account"
-  value       = module.storage.name
-}
