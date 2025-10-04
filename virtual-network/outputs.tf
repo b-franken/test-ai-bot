66f49db770618efc94028a92412ee6a1f4f4e7fe
Bas <basfrankenn@gmail.com>
2025-10-04 08:30:23 +0200

    Delete virtual-network directory

--- virtual-network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vnet_resource_id" {
-  description = "The resource ID of the virtual network."
-  value       = module.virtual_network.resource_id
-}
-
-output "vnet_name" {
-  description = "The name of the virtual network."
-  value       = module.virtual_network.name
-}
